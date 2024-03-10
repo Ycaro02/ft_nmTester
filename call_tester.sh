278f90a45b68372f1f953015aeb8ebd9ae046e48
Ycaro02 <ycaro@Kali.home>
2024-03-10 10:57:17 -0500

    take valgrind opt in call tester args same for bonus

--- call_tester.sh
+++ call_tester.sh
@@ -1,37 +1,31 @@
 #!/bin/bash
 
-TESTER="ft_nmTester/nm_tester.sh" 
+TESTER="rsc/nm_tester.sh" 
 NAME="ft_nm"
 
-./ft_nmTester/mk/ascii.sh "tester"
+VALGRIND_OPT=0
 
+if [ ! -z $1 ]; then
+    VALGRIND_OPT=1
+fi
 
-# ${TESTER} ${NAME} 1
+./rsc/mk/ascii.sh "tester"
 
-# ${TESTER} ${NAME} 0 "-p" "-p"
-# ${TESTER} ${NAME} 1 "--no-sort" "-p"
+${TESTER} ${NAME}
+${TESTER} ${NAME} ${VALGRIND_OPT}
 
-# ${TESTER} ${NAME} 0 "-r" "-r"
-# ${TESTER} ${NAME} 1 "-r" "-r"
-
-# ${TESTER} ${NAME} 0 "-u" "-u"
-# ${TESTER} ${NAME} 1 "-u" "-u"
-
-# ${TESTER} ${NAME} 0 "-g" "-g"
-# ${TESTER} ${NAME} 1 "-g" "-g"
-
-# ${TESTER} ${NAME} 0 "-a" "-a"
-${TESTER} ${NAME} 1 "-a" "-a"
-
-
-# ${TESTER} ${NAME} 1 "-rp" "-rp"
-# ${TESTER} ${NAME} 1 "-ru" "-ru"
-# ${TESTER} ${NAME} 1 "-pu" "-pu"
-
-# ${TESTER} ${NAME} 1 "-rpu" "-rpu"
-
-# ${TESTER} ${NAME} 1 "-gu" "-gu"
-# ${TESTER} ${NAME} 1 "-gp" "-gp"
-# ${TESTER} ${NAME} 1 "-gr" "-gr"
-
-# ${TESTER} ${NAME} 1 "-grpu" "-grpu"
+if [ ! -z $2 ]; then
+    ${TESTER} ${NAME} ${VALGRIND_OPT} "-p" "-p"
+    ${TESTER} ${NAME} ${VALGRIND_OPT} "-r" "-r"
+    ${TESTER} ${NAME} ${VALGRIND_OPT} "-u" "-u"
+    ${TESTER} ${NAME} ${VALGRIND_OPT} "-g" "-g"
+    ${TESTER} ${NAME} ${VALGRIND_OPT} "-a" "-a"
+    ${TESTER} ${NAME} ${VALGRIND_OPT} "-rp" "-rp"
+    ${TESTER} ${NAME} ${VALGRIND_OPT} "-ru" "-ru"
+    ${TESTER} ${NAME} ${VALGRIND_OPT} "-pu" "-pu"
+    ${TESTER} ${NAME} ${VALGRIND_OPT} "-rpu" "-rpu"
+    ${TESTER} ${NAME} ${VALGRIND_OPT} "-gu" "-gu"
+    ${TESTER} ${NAME} ${VALGRIND_OPT} "-gp" "-gp"
+    ${TESTER} ${NAME} ${VALGRIND_OPT} "-gr" "-gr"
+    ${TESTER} ${NAME} ${VALGRIND_OPT} "-grpu" "-grpu"
+fi
